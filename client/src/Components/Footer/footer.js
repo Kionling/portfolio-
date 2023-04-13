ff16c2ea22a452eb661ffe111b0a0457f5a3088e
Daniel Jauregui <djvdjv209@gmail.com>
2023-04-13 11:57:37 -0700

    Arranging HTML elements and created global class css for ease of use.

--- client/src/Components/Footer/footer.js
+++ client/src/Components/Footer/footer.js
@@ -1,10 +1,30 @@
 import React from "react";
+import "../Footer/footer.css"
+import { Link } from 'react-router-dom';
 
 
-function Footer(){
-    return(
+function Footer() {
+    return (
         <div id="footerContainer">
-            
+            <footer class="page-footer">
+                <div class="container">
+                    <h4 className="franklin">Daniel Jauregui</h4>
+                    <div className="row">
+                        
+                       
+
+                    </div>
+                </div>
+                <div class="footer-copyright">
+                    <div class="container white-text">
+                        © 2023 Daniel Jauregui
+                        <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+                    </div>
+                </div>
+            </footer>
+
         </div>
     )
 }
+
+export default Footer; 
