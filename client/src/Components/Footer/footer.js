aa136ad9d2176dd688542a4a6fbf71ba309ca882
Daniel Jauregui <djvdjv209@gmail.com>
2023-04-15 14:39:12 -0700

    Creating new architecture for footer content

--- client/src/Components/Footer/footer.js
+++ client/src/Components/Footer/footer.js
@@ -12,20 +12,23 @@ import Home from "../../Pages/Home/home"
 function Footer() {
     return (
         <div id="footerContainer" className="#424242 grey darken-3">
-            <footer class="page-footer">
+            <footer class="page-footer #424242 grey darken-3">
                 <div class="container">
-                    <h4 className="franklin">Daniel Jauregui</h4>
-                    <div className="row">
-                        <div className="col l3 franklin"><Link to="about" className="white-text">About</Link></div>
-                        <div className="col l3 franklin"><Link to="contact" className="white-text">Contact</Link></div>
-                        <div className="col l3 franklin"><Link to="projects" className="white-text">Projects</Link></div>
-                        <div className="col l3 franklin"><Link to="github" className="white-text">Github</Link></div>
+                    {/* <h4 className="franklin" id="footerTitle">More Links</h4> */}
+                    <div className="row" id="footerLI">
+                        <div className="col l2 franklin" id="aboutLI"><Link to="about" className="white-text">About</Link></div>
+                        <div className="col l2 franklin"><Link to="contact" className="white-text">Contact</Link></div>
+                        <div className="col l2 franklin"><Link to="projects" className="white-text">Projects</Link></div>
+                        <div className="col l2 franklin"><Link to="github" className="white-text">Github</Link></div>
                     </div>
                 </div>
-                <div class="footer-copyright">
-                    <div class="container white-text">
+                <div className="container">
+                    <p>Full Stack Developer/Software Engineer</p>
+                </div>
+                <div id="lowerSec" className="#424242 grey darken-3">
+                    <div class="container #424242 grey-text ">
                         © 2023 Daniel Jauregui
-                        <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+                        <a class="grey-text right" href="#!">More Links</a>
                     </div>
                 </div>
             </footer>
