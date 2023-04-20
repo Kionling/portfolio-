38beae6e5279fbde0cc162438defd33d3821ae79
Daniel Jauregui <djvdjv209@gmail.com>
2023-04-20 12:13:59 -0700

    Created function that creates navbar list items based on an array of objects

--- client/src/Components/Navbar/navBar.js
+++ client/src/Components/Navbar/navBar.js
@@ -1,49 +1,56 @@
-import { React, useEffect } from "react"
+import { React, useEffect, useState } from "react"
 import { Link, useLocation } from "react-router-dom"
 import "./navbar.css"
 
-function handleUrlChange() {
-    console.log('Current URL:', window.location.href);
-}
 
 function Navbar() {
-    const location = useLocation();
-    useEffect(() => {
-        handleUrlChange();
-    }, [location]);
-
+    const [navLinks, setNavLinks] = useState([
+        { path: '/portfolio', label: 'Portfolio', isActive: false },
+        { path: '/about', label: 'About', isActive: false },
+        { path: '/contact', label: 'Contact', isActive: false },
+      ]);
+      const location = useLocation();
+    //   console.log(isActive)
+    
+      useEffect(() => {
+        // Update the navLinks array based on the current URL path
+        const newNavLinks = navLinks.map((link) => {
+          if (link.path === location.pathname) {
+            return { ...link, isActive: true };
+          } else {
+            return { ...link, isActive: false };
+          }
+        });
+        setNavLinks(newNavLinks);
+      }, [location]);
+    
     return (
         <div>
-            <div class="container">
-                <div class="row">
-                    <div class="col s9">
+            <div className="container">
+                <div className="row">
+                    <div className="col s9">
 
                         <h3 id="nav-banner">
-                            <Link to="/" class="black-text">
+                            <Link to="/" className="black-text">
                                 Daniel Jauregui
                             </Link>
                         </h3>
                     </div>
                 </div>
-                <nav class="z-depth-0">
-                    <div class="nav-wrapper #ffffff white ">
-                        <a href="#" class="brand-logo center"></a>
-                        <ul id="nav-mobile" class="left">
-
-                            <li>
-                                <Link to="/Portfolio" className="black-text franklin">Portfolio</Link>
-                            </li>
-                            <li>
-                                <Link to="/about" className="black-text franklin">About</Link>
-                            </li>
-                            <li>
-                                <Link to="/contact" className="black-text franklin">Contact</Link>
-                            </li>
-
+                <nav className="z-depth-0">
+                    <div className="nav-wrapper #ffffff white ">
+                        <a href="#" className="brand-logo center"></a>
+                        <ul id="nav-mobile" className="left">
+                            {navLinks.map((link) => (
+                                <li key={link.path} className={link.isActive ? 'active' : 'false'}>
+                                    <Link to={link.path} className="black-text franklin">{link.label}</Link>
+                                </li>
+                            ))}
 
                         </ul>
                     </div>
                 </nav>
+
             </div>
         </div>
     )
