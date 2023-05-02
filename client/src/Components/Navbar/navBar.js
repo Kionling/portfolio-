a4054d595bb0f44bf2f8e21970df5b8abf02cef3
kionling <djvdjv209@gmail.com>
2023-05-02 00:14:22 -0700

    Designing the first banenr on the homepage as well as formatting the code to be readable

--- client/src/Components/Navbar/navBar.js
+++ client/src/Components/Navbar/navBar.js
@@ -1,60 +1,54 @@
-import { React, useEffect, useState } from "react"
-import { Link, useLocation } from "react-router-dom"
-import "./navbar.css"
-
+import { React, useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./navbar.css";
 
 function Navbar() {
-    const [navLinks, setNavLinks] = useState([
-        { path: '/portfolio', label: 'Portfolio',  className: "" },
-        { path: '/about', label: 'About',  className: ""},
-        { path: '/contact', label: 'Contact', className: "" },
-      ]);           
-      const location = useLocation();
-      useEffect(() => {
-        // Update the navLinks array based on the current URL path
-        const newNavLinks = navLinks.map((link) => {
-          if (link.path === location.pathname) {
-            return { ...link, className: "noActive" };
-          } else {
-            return { ...link, className: ""};
-          }
-        });
-        setNavLinks(newNavLinks);
-      }, [location]);
-         
-    
-    return (
-        <div>
-            <div className="container">
-                <div className="row">
-                    <div className="col s9">
+  const [navLinks, setNavLinks] = useState([
+    { path: "/portfolio", label: "Portfolio", className: "" },
+    { path: "/about", label: "About", className: "" },
+    { path: "/contact", label: "Contact", className: "" },
+  ]);
+  const location = useLocation();
+  useEffect(() => {
+    // Update the navLinks array based on the current URL path
+    const newNavLinks = navLinks.map((link) => {
+      if (link.path === location.pathname) {
+        return { ...link, className: "noActive" };
+      } else {
+        return { ...link, className: "" };
+      }
+    });
+    setNavLinks(newNavLinks);
+  }, [location]);
 
-                        <h3 id="nav-banner">
-                            <Link to="/" className="black-text">
-                                Daniel Jauregui
-                            </Link>
-                        </h3>
-                    </div>
-                </div>
-                <nav className="z-depth-0 #ffffff white">
-                    <div className="#ffffff white">
-                        
-                        <ul id="nav-mobile" className="left z-depth-0">
-                            {navLinks.map((link) => (
-                                <li key={link.path} className={link.className}>
-                                    <Link to={link.path} className="black-text bold z-depth-0">{link.label}</Link>
-                                </li>
-                            ))}
-
-                        </ul>
-                    </div>
-                </nav>
-
-            </div>
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col s9">
+            <h3 id="nav-banner">
+              <Link to="/" className="black-text">
+                Daniel Jauregui
+              </Link>
+            </h3>
+          </div>
         </div>
-    )
-
+        <nav className="z-depth-0 #ffffff white">
+          <div className="#ffffff white">
+            <ul id="nav-mobile" className="left z-depth-0">
+              {navLinks.map((link) => (
+                <li key={link.path} className={link.className}>
+                  <Link to={link.path} className="black-text bold z-depth-0">
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
 }
-
 
 export default Navbar;
--- client/src/Pages/Home/home.js
+++ client/src/Pages/Home/home.js
@@ -1,16 +1,20 @@
-import React from "react"
-import { Link } from "react-router-dom"
-import "./home.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./home.css";
 
 function Home() {
-    return (
-        <div id="mainBanner">
-            <div className="row bold centerText" id="firstBanText">
-            <h4 className="white-text bold" >The Newest Project<span id="period">.</span></h4>
-
-            </div>
+  return (
+    <div id="overallBanner">
+      <div id="mainBanner">
+        <div className="row bold centerText" id="firstBanText">
+          <h4 className="white-text bold">
+            The Newest Project<span id="period">.</span>
+          </h4>
         </div>
-    )
+      </div>
+      <div className="row #ff1744 red accent-3" id="firstBanner"></div>
+    </div>
+  );
 }
 
 export default Home;
