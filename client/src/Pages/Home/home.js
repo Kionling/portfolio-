e61c2fec07d7a486325a0ccaaee36318b6453f7f
Daniel Jauregui <djvdjv209@gmail.com>
2023-05-02 15:16:29 -0700

    Adding content to banners.

--- client/src/Pages/Home/home.js
+++ client/src/Pages/Home/home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./home.css";
-
+import nostalgia from "../Home/assets/nostalgia.jpg";
 function Home() {
   return (
     <div id="overallBanner">
@@ -13,10 +13,34 @@ function Home() {
         </div>
       </div>
       <div className="row #ff1744 red accent-3" id="firstBanner">
-        <div className="col l4 ">
-            <p className="bold">Explore</p>
+        <div className="col s4 l4">
+          <div className="container">
+            <p className="bold centerText" id="banTxt">
+              Explore
+            </p>
+          </div>
+        </div>
+        <div className=" col s8 l8" id="firstImage"></div>
+      </div>
+      <div className="row #aa00ff purple accent-4" id="secondBanner">
+        <div className="col s8 l8" id="secondImage"></div>
+        <div className=" container col s4 l4">
+          <p className=" bold centerText" id="banTxt">
+            Explore
+          </p>
         </div>
       </div>
+      <div className="row #651fff deep-purple accent-3" id="thirdBanner">
+        <div className="col s4 l4">
+          <div className="container">
+            <p className="bold centerText" id="banTxt">
+              Explore
+            </p>
+          </div>
+        </div>
+        <div className=" col s8 l8" id="firstImage"></div>
+      </div>
+      
     </div>
   );
 }
