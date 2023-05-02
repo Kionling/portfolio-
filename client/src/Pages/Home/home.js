be9cc8fe4b9f76588af06fdee993f8eaf16d74a5
kionling <djvdjv209@gmail.com>
2023-05-02 00:18:15 -0700

    Beginning intial design with explore tag that will redirect you to a different section of the home page.

--- client/src/Pages/Home/home.js
+++ client/src/Pages/Home/home.js
@@ -12,7 +12,11 @@ function Home() {
           </h4>
         </div>
       </div>
-      <div className="row #ff1744 red accent-3" id="firstBanner"></div>
+      <div className="row #ff1744 red accent-3" id="firstBanner">
+        <div className="col l4 ">
+            <p className="bold">Explore</p>
+        </div>
+      </div>
     </div>
   );
 }
