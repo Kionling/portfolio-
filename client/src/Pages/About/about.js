dd99590540c1544b6c415a15e2710c5ebded3f00
kionling <djvdjv209@gmail.com>
2023-05-03 13:14:54 -0700

    Starting to edit about page

--- client/src/Pages/About/about.js
+++ client/src/Pages/About/about.js
@@ -1,10 +1,12 @@
 import React from "react"
-
+import "../About/about.css"
 
 function About(){
     return(
         <div>
-            <h1>About</h1>
+            <div className="row #000000 black center" id="intro">
+                <h1 className="white-text bold centerText">My Name is Daniel Jauregui</h1>
+            </div>
         </div>
     )
 }
