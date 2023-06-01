b8a121f0080021d7c32b2bb1e62c92727ce305bc
Daniel Jauregui <djvdjv209@gmail.com>
2023-06-01 15:24:39 -0700

    Added final image to portfolio page

--- client/src/Pages/Portfolio/portfolio.js
+++ client/src/Pages/Portfolio/portfolio.js
@@ -1,38 +1,14 @@
 import React, { useEffect } from "react";
 import "./portfolio.css";
 import Danny from "./assets/Dan.jpg";
-import HIKR from "./assets/signinHikr.png";
 import PlanNJam from "./assets/planN.png";
-import EmployeeGen from "./assets/password.png";
-import PC from "./assets/pc.png";
+import PC from "./assets/PCPARTS.png";
 import Weather from "./assets/weatherDash.png";
 
 
 function Portfolio() {
   return (
     <div className="">
-      <div class="row">
-        <div class="col s12 m8 offset-m2 l6 offset-l3 ">
-          <div class="card-panel #f50057 pink accent-3 z-depth-0" id="cardDiv">
-            <div class="row valign-wrapper ">
-              <div class="col s2">
-                <img src={Danny} alt="Danny" class="circle responsive-img" />
-              </div>
-              <div class="col s10">
-                <span class="white-text" id="spanWeight">
-                  This is a message from the creator. As a part-time student and
-                  full-time father, these projects are highly self-driven. In
-                  order to complete these assignments, it requires dedication,
-                  self-motivation, and passion for which I am a student diving
-                  into the unknown. Below are my milestones in my career as a
-                  developer. I will showcase the top ones I loved to create. I
-                  will also link the rest of my projects through my Github!
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
       <div class="container">
         <div class="row ">
           <div class="col l4">
@@ -106,13 +82,13 @@ function Portfolio() {
           <div class="">
             <div class="row">
               <div class="col l4">
-                <div class="card  #f48fb1 pink lighten-3 ">
+                <div class="card #f48fb1 pink lighten-3 ">
                   <div class="card-image waves-effect waves-block waves-light">
                     <img class="activator" src={PC} alt="pcxparts" />
                   </div>
                   <div class="card-content">
                     <span class="card-title activator grey-text text-darken-4">
-                      Pc parts wishlist
+                      Pc Parts Wishlist
                       <i class="material-icons right activator">more_vert</i>
                     </span>
                     <p>
@@ -139,7 +115,28 @@ function Portfolio() {
           </div>
         </div>
       </div>
-
+      <div class="row">
+        <div class="col s12 m8 offset-m2 l6 offset-l3 ">
+          <div class="card-panel #f50057 pink accent-3 z-depth-0" id="cardDiv">
+            <div class="row valign-wrapper ">
+              <div class="col s2">
+                <img src={Danny} alt="Danny" class="circle responsive-img" />
+              </div>
+              <div class="col s10">
+                <span class="white-text" id="spanWeight">
+                  This is a message from the creator. As a part-time student and
+                  full-time father, these projects are highly self-driven. In
+                  order to complete these assignments, it requires dedication,
+                  self-motivation, and passion for which I am a student diving
+                  into the unknown. Below are my milestones in my career as a
+                  developer. I will showcase the top ones I loved to create. I
+                  will also link the rest of my projects through my Github!
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div class="parallax-container" id="wantToSee">
         <div id="parallaxText">
           <h1 id="seeMore">
